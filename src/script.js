b8b5681b0c567b7fb1731e6403b70fda940aaa9f
samia-ctrl <samiauddin99@gmail.com>
2023-07-01 16:44:20 +0100

    question array

--- src/script.js
+++ src/script.js
@@ -1,13 +1,38 @@
-function alertAnswer(event) {
-  let feedback = document.querySelector("#response-to-answer");
-  feedback.innerHTML = "Correct ✅";
-}
-let leftButton = document.querySelector("#opt-1");
-leftButton.addEventListener("click", alertAnswer);
-
-function alertResponse(event) {
-  let feedbackResponse = document.querySelector("#response-to-answer");
-  feedbackResponse.innerHTML = "Incorrect ❌";
-}
-let rightButton = document.querySelector("#opt-2");
-rightButton.addEventListener("click", alertResponse);
+let questions = [
+  {
+    question: "What is the square root of 16?",
+    answers: [
+      { text: "8", correct: false },
+      { text: "4", correct: true },
+      { text: "16", correct: false },
+      { text: "2", correct: false },
+    ],
+  },
+  {
+    question: "What is the square root of 64?",
+    answers: [
+      { text: "8", correct: true },
+      { text: "4", correct: false },
+      { text: "16", correct: false },
+      { text: "2", correct: false },
+    ],
+  },
+  {
+    question: "What is 7²?",
+    answers: [
+      { text: "14", correct: false },
+      { text: "42", correct: false },
+      { text: "52", correct: false },
+      { text: "49", correct: true },
+    ],
+  },
+  {
+    question: "What is 9²?",
+    answers: [
+      { text: "99", correct: false },
+      { text: "90", correct: false },
+      { text: "18", correct: false },
+      { text: "81", correct: true },
+    ],
+  },
+];
